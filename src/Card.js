4823023e3c7134cc323810cae2679477824e8651
questm <77356948+questm@users.noreply.github.com>
2022-04-24 18:03:08 -0400

    commit on 'master'

---
+++ src/Card.js
@@ -0,0 +1,19 @@
+import './Card.css';
+
+
+
+
+const Card = ({ userData }) => {
+    return (
+        <div className="card">
+            <div className="card__title">{userData.name.first} {userData.name.last}</div>
+            <div className="card__body">
+                
+                <div className="card__image"><img src={userData.picture.medium}/></div>
+            </div>
+
+        </div>
+    )
+};
+
+export default Card;
